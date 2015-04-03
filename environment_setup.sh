060fa3c27f0eb857415544be2f8f2f5134477570
Jaymz Rhime <wxactly@gmail.com>
2015-04-03 14:57:19 -0700

    Update script with all of the set up from the ThinkShout brew/lamp doc

--- environment_setup.sh
+++ environment_setup.sh
@@ -18,6 +18,23 @@ confirmupdate () {
   esac
 }
 
+xcode_path=`xcode-select -p`
+echo $'\n'
+echo "Sets up the standard ThinkShout development environment."
+echo $'\n'
+echo "Work-in-progress."
+echo $'\n'
+echo "Prereq: Must be running OSX 10.10 Yosemite"
+echo $'\n'
+echo "Prereq: Xcode must be installed with Command Line Tools. (xcode-select path currently = $xcode_path)"
+echo $'\n'
+
+if confirmupdate "Would you like to proceed?"; then
+  echo "Starting setup..."
+else
+  exit
+fi
+
 # Homebrew
 
 # Check Homebrew is installed.
@@ -47,40 +64,232 @@ fi
 brew tap homebrew/dupes
 brew tap homebrew/homebrew-php
 
-path_export='export PATH="#{HOMEBREW_PREFIX}/bin:$PATH"'
-
-echo "Which shell?"
-echo "1) Bash"
-echo "2) Zsh"
-echo "3) Other"
-read -r -p "" shell
-case $shell in
-  [1])
-    echo "Updated ~/.bashrc"
-    echo "$path_export" >> ~/.bashrc
-    ;;
-  [2])
-    echo "Updated ~/.zshrc"
-    echo "$path_export" >> ~/.zshrc
-	;;
-  *)
-  echo "Add the following to your shell profile:"
-  echo path_export
-esac
-
 echo $'\n'
 echo "Installing dev tools via Homebrew"
 echo $'\n'
+
+# git
+
+brew install git
 
 # wget
 
 brew install wget
 
-# LAMP
+# MySQL
 
-# TODO: Make PHP version optional (PHP 5.3 / PHP 5.4 at least)
-brew install php55
 brew install mysql
+
+cp -v $(brew --prefix mysql)/support-files/my-default.cnf $(brew --prefix)/etc/my.cnf
+
+cat >> $(brew --prefix)/etc/my.cnf <<'EOF'
+
+# Echo & Co. changes
+max_allowed_packet = 1073741824
+innodb_file_per_table = 1
+EOF
+
+sed -i '' 's/^#[[:space:]]*\(innodb_buffer_pool_size\)/\1/' $(brew --prefix)/etc/my.cnf
+
+[[ ! -d ~/Library/LaunchAgents ]] && mkdir -v ~/Library/LaunchAgents
+
+ln -sfv $(brew --prefix mysql)/homebrew.mxcl.mysql.plist ~/Library/LaunchAgents/
+
+launchctl load -Fw ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+
+# Apache
+
+sudo launchctl unload /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
+
+brew install -v httpd22 --with-brewed-openssl
+
+[ ! -d ~/Sites ] && mkdir -pv ~/Sites
+
+mkdir -pv ~/Sites/{logs,ssl}
+
+touch ~/Sites/httpd-vhosts.conf
+
+USERHOME=$(dscl . -read /Users/`whoami` NFSHomeDirectory | awk -F": " '{print $2}') cat >> $(brew --prefix)/etc/apache2/2.2/httpd.conf <<EOF
+# Include our VirtualHosts
+Include ${USERHOME}/Sites/httpd-vhosts.conf
+EOF
+
+(export USERHOME=$(dscl . -read /Users/`whoami` NFSHomeDirectory | awk -F"\: " '{print $2}') ; cat > ~/Sites/httpd-vhosts.conf <<EOF
+#
+# Listening ports.
+#
+#Listen 8080  # defined in main httpd.conf
+Listen 8443
+
+#
+# Use name-based virtual hosting.
+#
+NameVirtualHost *:8080
+NameVirtualHost *:8443
+
+#
+# Set up permissions for VirtualHosts in ~/Sites
+#
+<Directory "${USERHOME}/Sites">
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride All
+    <IfModule mod_authz_core.c>
+        Require all granted
+    </IfModule>
+    <IfModule !mod_authz_core.c>
+        Order allow,deny
+        Allow from all
+    </IfModule>
+</Directory>
+
+# For http://localhost in the users' Sites folder
+<VirtualHost _default_:8080>
+    ServerName localhost
+    DocumentRoot "${USERHOME}/Sites"
+</VirtualHost>
+<VirtualHost _default_:8443>
+    ServerName localhost
+    Include "${USERHOME}/Sites/ssl/ssl-shared-cert.inc"
+    DocumentRoot "${USERHOME}/Sites"
+</VirtualHost>
+
+#
+# VirtualHosts
+#
+
+## Manual VirtualHost template for HTTP and HTTPS
+#<VirtualHost *:8080>
+#  ServerName project.dev
+#  CustomLog "${USERHOME}/Sites/logs/project.dev-access_log" combined
+#  ErrorLog "${USERHOME}/Sites/logs/project.dev-error_log"
+#  DocumentRoot "${USERHOME}/Sites/project.dev"
+#</VirtualHost>
+#<VirtualHost *:8443>
+#  ServerName project.dev
+#  Include "${USERHOME}/Sites/ssl/ssl-shared-cert.inc"
+#  CustomLog "${USERHOME}/Sites/logs/project.dev-access_log" combined
+#  ErrorLog "${USERHOME}/Sites/logs/project.dev-error_log"
+#  DocumentRoot "${USERHOME}/Sites/project.dev"
+#</VirtualHost>
+
+#
+# Automatic VirtualHosts
+#
+# A directory at ${USERHOME}/Sites/webroot can be accessed at http://webroot.dev
+# In Drupal, uncomment the line with: RewriteBase /
+#
+
+# This log format will display the per-virtual-host as the first field followed by a typical log line
+LogFormat "%V %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combinedmassvhost
+
+# Auto-VirtualHosts with .dev
+<VirtualHost *:8080>
+  ServerName dev
+  ServerAlias *.dev
+
+  CustomLog "${USERHOME}/Sites/logs/dev-access_log" combinedmassvhost
+  ErrorLog "${USERHOME}/Sites/logs/dev-error_log"
+
+  VirtualDocumentRoot ${USERHOME}/Sites/%-2+
+</VirtualHost>
+<VirtualHost *:8443>
+  ServerName dev
+  ServerAlias *.dev
+  Include "${USERHOME}/Sites/ssl/ssl-shared-cert.inc"
+
+  CustomLog "${USERHOME}/Sites/logs/dev-access_log" combinedmassvhost
+  ErrorLog "${USERHOME}/Sites/logs/dev-error_log"
+
+  VirtualDocumentRoot ${USERHOME}/Sites/%-2+
+</VirtualHost>
+EOF
+)
+
+(export USERHOME=$(dscl . -read /Users/`whoami` NFSHomeDirectory | awk -F"\: " '{print $2}') ; cat > ~/Sites/ssl/ssl-shared-cert.inc <<EOF
+SSLEngine On
+SSLProtocol all -SSLv2 -SSLv3
+SSLCipherSuite ALL:!ADH:!EXPORT:!SSLv2:RC4+RSA:+HIGH:+MEDIUM:+LOW
+SSLCertificateFile "${USERHOME}/Sites/ssl/selfsigned.crt"
+SSLCertificateKeyFile "${USERHOME}/Sites/ssl/private.key"
+EOF
+)
+
+openssl req \
+  -new \
+  -newkey rsa:2048 \
+  -days 3650 \
+  -nodes \
+  -x509 \
+  -subj "/C=US/ST=State/L=City/O=Organization/OU=$(whoami)/CN=*.dev" \
+  -keyout ~/Sites/ssl/private.key \
+  -out ~/Sites/ssl/selfsigned.crt
+
+ln -sfv $(brew --prefix httpd22)/homebrew.mxcl.httpd22.plist ~/Library/LaunchAgents
+
+launchctl load -Fw ~/Library/LaunchAgents/homebrew.mxcl.httpd22.plist
+
+sudo bash -c 'export TAB=$'"'"'\t'"'"'
+cat > /Library/LaunchDaemons/co.echo.httpdfwd.plist <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+${TAB}<key>Label</key>
+${TAB}<string>co.echo.httpdfwd</string>
+${TAB}<key>ProgramArguments</key>
+${TAB}<array>
+${TAB}${TAB}<string>sh</string>
+${TAB}${TAB}<string>-c</string>
+${TAB}${TAB}<string>echo "rdr pass proto tcp from any to any port {80,8080} -> 127.0.0.1 port 8080" | pfctl -a "com.apple/260.HttpFwdFirewall" -Ef - &amp;&amp; echo "rdr pass proto tcp from any to any port {443,8443} -> 127.0.0.1 port 8443" | pfctl -a "com.apple/261.HttpFwdFirewall" -Ef - &amp;&amp; sysctl -w net.inet.ip.forwarding=1</string>
+${TAB}</array>
+${TAB}<key>RunAtLoad</key>
+${TAB}<true/>
+${TAB}<key>UserName</key>
+${TAB}<string>root</string>
+</dict>
+</plist>
+EOF'
+
+sudo launchctl load -Fw /Library/LaunchDaemons/co.echo.httpdfwd.plist
+
+# PHP
+# TODO: Make PHP version optional (PHP 5.3 / PHP 5.4 at least)
+
+brew install php55 --homebrew-apxs --with-apache
+
+cat >> $(brew --prefix)/etc/apache2/2.2/httpd.conf <<EOF
+# Send PHP extensions to mod_php
+AddHandler php5-script .php
+AddType text/html .php
+DirectoryIndex index.php index.html
+EOF
+
+sed -i '-default' "s|^;\(date\.timezone[[:space:]]*=\).*|\1 \"$(sudo systemsetup -gettimezone|awk -F": " '{print $2}')\"|; s|^\(memory_limit[[:space:]]*=\).*|\1 256M|; s|^\(post_max_size[[:space:]]*=\).*|\1 200M|; s|^\(upload_max_filesize[[:space:]]*=\).*|\1 100M|; s|^\(default_socket_timeout[[:space:]]*=\).*|\1 600|; s|^\(max_execution_time[[:space:]]*=\).*|\1 300|; s|^\(max_input_time[[:space:]]*=\).*|\1 600|;" $(brew --prefix)/etc/php/5.5/php.ini
+
+USERHOME=$(dscl . -read /Users/`whoami` NFSHomeDirectory | awk -F": " '{print $2}') cat >> $(brew --prefix)/etc/php/5.5/php.ini <<EOF
+; PHP Error log
+error_log = ${USERHOME}/Sites/logs/php-error_log
+EOF
+
+touch $(brew --prefix php55)/lib/php/.lock && chmod 0644 $(brew --prefix php55)/lib/php/.lock
+
+/usr/bin/sed -i '' "s|^\(\;\)\{0,1\}[[:space:]]*\(opcache\.enable[[:space:]]*=[[:space:]]*\)0|\21|; s|^;\(opcache\.memory_consumption[[:space:]]*=[[:space:]]*\)[0-9]*|\1256|;" $(brew --prefix)/etc/php/5.5/php.ini
+
+# Dnsmasq
+
+brew install dnsmasq
+
+echo 'address=/.dev/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.conf
+echo 'listen-address=127.0.0.1' >> $(brew --prefix)/etc/dnsmasq.conf
+echo 'port=35353' >> $(brew --prefix)/etc/dnsmasq.conf
+
+ln -sfv $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist ~/Library/LaunchAgents
+
+launchctl load -Fw ~/Library/LaunchAgents/homebrew.mxcl.dnsmasq.plist
+
+sudo mkdir -v /etc/resolver
+sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/dev'
+sudo bash -c 'echo "port 35353" >> /etc/resolver/dev'
 
 # Drush
 
@@ -90,36 +299,13 @@ brew install drush
 
 brew install composer
 
-# TODO: Set up mysql to autostart
-# TODO: Configure mysql for concurrent connections
-
 # Xdebug
-# TODO: Automate configuration in Apache config (might be done by brew.)
 
 brew install php55-xdebug
 
 # Drupal Code Sniffer
 
 brew install drupal-code-sniffer
-
-# Apache vhosts
-
-if confirmupdate "Download example httpd-vhosts.conf file?"; then
-  wget https://github.com/thinkshout/ts_recipes/blob/master/brew-lamp-dev-envt/httpd-vhosts.conf
-  mv httpd-vhosts.conf ~/Sites/
-  sudo mv /private/etc/apache2/extra/httpd-vhosts.conf /private/etc/apache2/extra/httpd-vhosts.conf.bak
-  sudo ln -s ~/Sites/httpd-vhosts.conf /private/etc/apache2/extra/httpd-vhosts.conf
-else
-  echo $'\n'
-  echo "Make sure to symlink your httpd-vhosts.conf file to /private/etc/apache2/extra/httpd-vhosts.conf"
-  echo $'\n'
-fi
-
-# Hosts
-
-if confirmupdate "Create example item in hosts file?"; then
-  sudo echo "127.0.0.1 site.local" >> /private/etc/hosts
-fi
 
 # Restart Apache
 
