4ea8a1031d63ad270933fc89066e885db59bf775
Greg Boggs <me@gregboggs.com>
2016-04-26 11:03:04 -0700

    Update terminus-download-backup.sh

--- shell-snippets/terminus-download-backup.sh
+++ shell-snippets/terminus-download-backup.sh
@@ -1,4 +1,5 @@
-# Add this to your .bash_profile or other shell file, and then you can call 'tbg [sitename]' to download the latest db backup from pantheon in one step
+# Add this to your .bash_profile or other shell file, and then you can call 'tbg [sitename]' to download
+# the latest db backup from pantheon in one step
 tbg() {
   dburl=$(terminus site backups get --site="$1" --env="live" --latest --element="db"); 
   wget -Odatabase.sql.gz "$dburl" ; 
