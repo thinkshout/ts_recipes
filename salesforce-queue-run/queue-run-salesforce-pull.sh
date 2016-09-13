a5688a6c844f97c08d29141b9d4460c96bf30c2b
Jaymz Rhime <wxactly@gmail.com>
2016-09-12 22:34:32 -0500

    Add queue runner script for salesforce_pull

---
+++ salesforce-queue-run/queue-run-salesforce-pull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run through the salesforce_pull queue ad infinitum. Ctrl-C to stop.
+#
+# Usage:
+# $ ./queue-run-salesforce-pull.sh @pantheon.site.live >> queue-run-salesforce-pull.log
+
+if [ -z "$1" ]; then
+  echo "Missing drush site alias."
+  exit
+fi
+
+while true
+do
+  date
+  drush $1 queue-run salesforce_pull --time-limit=30
+	sleep 10
+done
+
