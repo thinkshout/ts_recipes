437436daed1c946b99e20a0f5438335b31398ef4
Olivier Bouwman <olivier@thinkshout.com>
2017-09-19 18:25:16 -0700

    security_updates_monitor script: make security advisory check work for all versions between current and new, fix bugs, change layout a bit

--- security_updates_monitor/security-updates-monitor.sh
+++ security_updates_monitor/security-updates-monitor.sh
@@ -1,6 +1,7 @@
 sites=`drush sa`
-# dev_sites=`echo "$sites" | grep '\.dev$'`
-dev_sites=`echo "$sites" | grep '\.givingforum.dev$'`
+dev_sites=`echo "$sites" | grep '\.dev$'`
+# dev_sites=`echo "$sites" | grep '\.givingforum.dev$'`
+# dev_sites=`echo "$sites" | grep '\.nastad.dev$'`
 
 while read -r line; do
 
@@ -31,61 +32,94 @@ while read -r line; do
         # loop through security updates
         while read -r item; do
 
+          item_part=`echo $item | sed -e 's/SECURITY UPDATE available//' | xargs`
+          # echo "item_part: $item_part"
+
+          # get new version
+          version_new=`echo "$item_part" | sed 's#.*\ ##g' | xargs`
+          version_new_minor=`echo "$version_new" | sed 's#.*\.##g'`
+          version_new_before_minor=`echo "$version_new" | sed 's/\(.*\)\..*/\1/'`
+          # echo "version_new: $version_new"
+          # echo "version_new_before_minor: $version_new_before_minor"
+          # echo "version_new_minor: $version_new_minor"
+
+          item_part=`echo $item_part | sed 's/\ [^ ]*$//' | xargs`
+          # echo "item_part: $item_part"
+
+          # get current version
+          version_current=`echo "$item_part" | sed 's#.*\ ##g' | xargs`
+          version_current_minor=`echo "$version_current" | sed 's#.*\.##g'`
+          version_current_before_minor=`echo "$version_current" | sed 's/\(.*\)\..*/\1/'`
+          # echo "version_current: $version_current"
+          # echo "version_current_before_minor: $version_current_before_minor"
+          # echo "version_current_minor: $version_current_minor"
+
+          item_part=`echo $item_part | sed 's/\ [^ ]*$//'`
+          # echo "item_part: $item_part"
+
           # get project machine name
-          machine_name=`echo "$item" | awk -F"[()]" '{print $2}'`
+          machine_name=`echo "$item_part" | awk -F"[()]" '{print $2}'`
 
           # couldn't find machine name, try to make it from the full name
           if [[ -z "$machine_name" ]]; then
-            machine_name=`echo "$item" | cut -c1-12`
+
+            machine_name=`echo "$item_part"`
             #remove white space
             machine_name=`echo "${machine_name//[[:space:]]/}"`
             #make lowercase
             machine_name=`echo "$machine_name" | tr '[:upper:]' '[:lower:]'`
           fi
-          echo "machine name: $machine_name"
+          # echo "-machine name: $machine_name"
 
-          # get current version
-          version_current=`echo "$item" | cut -c20-30 | xargs`
-          echo "version_current: $version_current"
-
-          # get new version
-          version_new=`echo "$item" | cut -c31-40 | xargs`
-          echo "version_new: $version_new"
-
+          # find url for changelog rss
           releases_url_rss=`curl -s https://www.drupal.org/project/$machine_name/releases | grep '<div id="feeds">' | sed -n 's/.*href="\([^"]*\).*/\1/p'`
-          echo "releases_url_rss: $releases_url_rss"
+          # echo "releases_url_rss: $releases_url_rss"
 
-          # find description for specific version
-          item_description=`curl -s "$releases_url_rss" | xmllint --xpath "//channel/item[title/text() = '$machine_name $version_new']/description/text()" -`
-          # echo "$item_description"
+          echo "- Security update available for: $machine_name (current: $version_current / new: $version_new)"
 
-          # find link for specific version
-          release_version_url=`curl -s "$releases_url_rss" | xmllint --xpath "//channel/item[title/text() = '$machine_name $version_new']/link/text()" -`
-          echo "release_version_url: $release_version_url"
+          # loop through minor versions (current + 1 to new)
+          startloop=`echo $((version_current_minor+1))`
+          for check_minor_version in $(eval echo "{$startloop..$version_new_minor}")
+          do
+          	# echo "check_minor_version: $check_minor_version"
+            check_version=`echo "$version_current_before_minor.$check_minor_version"`
+            # echo "check_version: $check_version"
 
-          # decode html entities in description
-          item_description=`echo "$item_description" | perl -MHTML::Entities -pe 'decode_entities($_);'`
-          # echo "$item_description"
+            # find description for specific version
+            item_description=`curl -s "$releases_url_rss" | xmllint --xpath "//channel/item[title/text() = '$machine_name $check_version']/description/text()" -`
+            # echo "$item_description"
 
-          # find url of security advisory
-          regex="<a href=\"(.*)\" .*>SA-.*<\/a>"
-          if [[ $item_description =~ $regex ]]; then
-            sa_url="${BASH_REMATCH[1]}"
-            echo "sa_url: $sa_url"
+            # find link for specific version
+            release_version_url=`curl -s "$releases_url_rss" | xmllint --xpath "//channel/item[title/text() = '$machine_name $check_version']/link/text()" -`
+            # echo "release_version_url: $release_version_url"
 
-            # find security risk level on security advisory page
-            sa_page=`curl -s "$sa_url" | grep risk-levels`
-            # echo "$sa_page"
-            regex="<a href=\"https:\/\/www\.drupal\.org\/security-team\/risk-levels\">.*\">(.*)<\/span>"
-            if [[ $sa_page =~ $regex ]]; then
-              sa_level="${BASH_REMATCH[1]}"
-              sa_level=`echo "$sa_level" | xargs`
-              echo "sa_level: $sa_level"
+            # decode html entities in description
+            item_description=`echo "$item_description" | perl -CS -MHTML::Entities -pe 'decode_entities($_);'`
+            # echo "$item_description"
+
+            # find url of security advisory
+            regex="<a href=\"(.*)\" .*>SA-.*<\/a>"
+            if [[ $item_description =~ $regex ]]; then
+              sa_url="${BASH_REMATCH[1]}"
+              # echo "sa_url: $sa_url"
+
+              # find security risk level on security advisory page
+              sa_page=`curl -s "$sa_url" | grep risk-levels`
+              # echo "$sa_page"
+              regex="<a href=\"https:\/\/www\.drupal\.org\/security-team\/risk-levels\">.*\">(.*)<\/span>"
+              if [[ $sa_page =~ $regex ]]; then
+                sa_level="${BASH_REMATCH[1]}"
+                sa_level=`echo "$sa_level" | xargs`
+                # echo "sa_level: $sa_level"
+                echo "  ! Security advisory Level: $sa_level ($sa_url)"
+              fi
             fi
-          fi
+          done
 
         done <<< "$security_update"
-        echo "$security_update"
+        # echo "-----original notice-----"
+        # echo "$security_update"
+        # echo "-----original notice-----"
       else
         # echo $result
         echo "- unknown error"
